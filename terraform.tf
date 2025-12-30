6a3899eb6e9bf270c9afe42837cdb48382e07722
balusena <balusena12@gmail.com>
2025-12-30 21:35:56 +0000

    add files to roboshop-config-values.

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,17 @@
+# ----------------------------
+# Vault Provider Configuration
+# ----------------------------
+# Configures Terraform to connect to a Vault server
+
 provider "vault" {
   address = "http://vault-internal.robobal.store:8200"
   token   = var.token
 }
+
+# ---------------------------------------
+# 2. Terraform Backend Configuration (S3)
+# ---------------------------------------
+# Configures remote state storage in an S3 bucket
 
 terraform {
   backend "s3" {
